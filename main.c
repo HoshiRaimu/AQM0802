41af85aa375085a1dad546dee0209fec073a0880
HoshiRaimu <m7juklo@gmail.com>
2021-03-16 15:21:36 +0900

    基本的な関数が完成

--- main.c
+++ main.c
@@ -26,14 +26,29 @@
 
 #include <xc.h>
 #include <stdint.h>
+#include <stdio.h>
+
+#define _XTAL_FREQ 4000000
 
 #define I2C_ACK 0x00
 #define I2C_NACK 0xff
+#define LCD_I2C_ADDRESS 0x7c
 
-void i2cProtocolStart();
-void i2cProtocolStop();
-void i2cProtocolSendData(uint8_t);
-uint8_t i2cProtocolCheckAck();
+// LCDモジュール表示制御関数
+void lcdInitialize(void);               // LCD初期化
+void lcdClearDisplay(void);             // ディスプレイ全消去
+void lcdSendCommandData(uint8_t);       // コマンド送信
+void lcdSendCharacterData(uint8_t);     // 1文字表示
+void lcdLocateCursor(uint8_t,uint8_t);  // カーソル位置指定
+ 
+// LCDモジュールI2Cプロトコル関数
+void lcdI2CProtocol(uint8_t, uint8_t, uint8_t);
+ 
+// I2Cプロトコル各信号の生成関数
+void    i2cProtocolStart(void);        // スタートビット生成
+void    i2cProtocolStop(void);         // ストップビット生成
+void    i2cProtocolSendData(uint8_t);  // 1バイトデータ送信
+uint8_t i2cProtocolCheckAck(void);     // ACK信号チェック
 
 void main(void) {
     //PICマイコンの設定
@@ -46,6 +61,23 @@ void main(void) {
     SSP1CON1 = 0x28;   // I2C通信のマスターモードを有効化
     SSP1CON3 = 0x00;   // CON3はデフォルト設定
     SSP1ADD  = 0x09;   //クロック信号速度を100kHzに設定
+    
+    // LCDモジュール電源安定化時間待ち
+    __delay_ms(100);
+     
+    // LCD初期化
+    lcdInitialize();
+ 
+    // LCD表示位置を左上に設定
+    lcdLocateCursor(1, 1);
+    printf("Hello");
+    
+    lcdLocateCursor(1, 2);
+    printf("World");
+ 
+    while(1){
+    }
+    
     return;
 }
 
@@ -97,4 +129,76 @@ uint8_t i2cProtocolCheckAck() {
     }
     
     return ack_status;
+}
+
+void lcdI2CProtocol(uint8_t address, uint8_t cont_code, uint8_t data) {
+    i2cProtocolStart();                 //スタートコンディション
+    i2cProtocolSendData(address);       //アドレス送信
+    i2cProtocolSendData(cont_code);     //制御コード送信
+    i2cProtocolSendData(data);          //データ送信
+    i2cProtocolStop();                  //ストップコンディション
+    
+    return;
+}
+
+void lcdSendCommandData(uint8_t command) {
+    // コマンドを送信する場合の制御コードは0x00
+    lcdI2CProtocol(LCD_I2C_ADDRESS, 0x00, command);
+    
+    // ウエイト
+    //   データシートではウエイト時間は26.3us以上になっているが、
+    //   それより長くしないと初期化できないケースがあるため1msのウエイトを入れる
+    __delay_ms(1);
+    
+    return;
+}
+
+void lcdSendCharacterData(uint8_t data) {
+    // 表示文字のデータを送信する場合の制御コードは0x40
+    lcdI2CProtocol(LCD_I2C_ADDRESS, 0x40, data);
+    
+    // ウエイト
+    //   文字表示の場合はウエイトを入れなくても動作しているが
+    //   表示されない場合は1ms程度のウエイトを入れる
+    // __delay_ms(1);
+    
+    return;
+}
+
+void lcdInitialize() {
+    // 初期化コマンド送信
+    lcdSendCommandData(0x38); // 2行モードに設定
+    lcdSendCommandData(0x39); // 拡張コマンド選択
+    lcdSendCommandData(0x14); // 内部クロック周波数設定
+    lcdSendCommandData(0x70); // コントラスト設定(C3:C0 = 0b0000に設定)
+    lcdSendCommandData(0x56); // 電源電圧が3.3VなのでBooster=ONに設定。コントラスト設定はC5:C4 = 0b10
+    lcdSendCommandData(0x6c); // オペアンプのゲイン設定
+    
+    // モジュール内電源安定化のための時間待ち
+    __delay_ms(200);
+    
+    // 初期化コマンド続き
+    lcdSendCommandData(0x38); // 通常コマンド選択
+    lcdSendCommandData(0x0c); // ディスプレイ表示
+    lcdSendCommandData(0x01); // ディスプレイ表示内容クリア
+    
+    return;
+}
+
+void lcdClearDisplay() {
+    lcdSendCommandData(0x01);
+    
+    return;
+}
+
+void lcdLocateCursor(uint8_t pos_x, uint8_t pos_y) {
+    lcdSendCommandData(0x80 + 0x40 * (pos_y - 1) + (pos_x - 1));
+    
+    return;
+}
+
+void putch(char character) {
+    lcdSendCharacterData(character);
+    
+    return;
 }
